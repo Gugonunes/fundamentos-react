08a27800d494078c79fd200942c0700a5200a0d6
Gugonunes <gugonunes@hotmail.com>
2025-01-13 14:00:27 -0300

    feat(layout): add new Card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,35 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from './components/layout/Card'
 
 const myApp = (props) => {
     return (
         <div id="app">
-            <Aleatorio
-                min={0}
-                max={10}>
-            </Aleatorio>
-            <Fragmento />
-            <ComParametro
-                titulo="Situação do aluno"
-                aluno="Pedro"
-                nota={9.3}>
-            </ComParametro>
-            <Primeiro />
+            <h1>Fundamentos React</h1>
+            <Card
+            titulo="#4 Desafio Aleatorio">
+                <Aleatorio
+                    min={0}
+                    max={10}>
+                </Aleatorio>
+            </Card>
+            <Card
+            titulo="#3 Fragmento">
+                <Fragmento />
+            </Card>
+            <Card
+            titulo="#2 Com parametro">
+                <ComParametro
+                    titulo="Situação do aluno"
+                    aluno="Pedro"
+                    nota={9.3}>
+                </ComParametro>
+            </Card>
+            <Card
+            titulo="#1 Primeiro">
+                <Primeiro />
+            </Card>
         </div>
     )
 }
