b0878f86f2d48153da7219e7d74518b9ecc7ef75
Gugonunes <gugonunes@hotmail.com>
2025-01-14 09:19:25 -0300

    style(app): add flex style to cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,37 @@ import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
 import Card from './components/layout/Card'
+import './App.css'
 
 const myApp = (props) => {
     return (
-        <div id="app">
+        <div className="App">
             <h1>Fundamentos React</h1>
-            <Card
-            titulo="#4 Desafio Aleatorio">
-                <Aleatorio
-                    min={0}
-                    max={10}>
-                </Aleatorio>
-            </Card>
-            <Card
-            titulo="#3 Fragmento">
-                <Fragmento />
-            </Card>
-            <Card
-            titulo="#2 Com parametro">
-                <ComParametro
-                    titulo="Situação do aluno"
-                    aluno="Pedro"
-                    nota={9.3}>
-                </ComParametro>
-            </Card>
-            <Card
-            titulo="#1 Primeiro">
-                <Primeiro />
-            </Card>
+            <div className="Cards">
+                <Card
+                    titulo="#4 Desafio Aleatorio">
+                    <Aleatorio
+                        min={0}
+                        max={10}>
+                    </Aleatorio>
+                </Card>
+                <Card
+                    titulo="#3 Fragmento">
+                    <Fragmento />
+                </Card>
+                <Card
+                    titulo="#2 Com parametro">
+                    <ComParametro
+                        titulo="Situação do aluno"
+                        aluno="Pedro"
+                        nota={9.3}>
+                    </ComParametro>
+                </Card>
+                <Card
+                    titulo="#1 Primeiro">
+                    <Primeiro />
+                </Card>
+            </div>
         </div>
     )
 }
